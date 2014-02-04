0e2a5df9d6d21f1261c91f98efa3d24edd52f279
Max Bogue <max@maxbogue.com>
2014-02-04 05:05:00 +0000

    Reorganize Editor.hs into sections.

--- src/Anvil/UI/Editor.hs
+++ src/Anvil/UI/Editor.hs
@@ -41,6 +41,16 @@ initEditor = do
     initMaterialElements
     initItemTypeElements
 
+editorItem :: Fay Item
+editorItem = do
+    itemType <- querySelector ".itemType.selected" >>= getAnvilValue
+    material <- querySelector ".material.selected" >>= getAnvilValue
+    name <- querySelector "#name" >>= getStringValue
+    let nnj = if name == "" then Right 0 else Left name
+    return $ makeItem itemType material [] nnj
+
+-- ItemType selection logic.
+
 initItemTypeElements :: Fay ()
 initItemTypeElements = do
     elements <- getElementsByClass "itemType"
@@ -54,6 +64,15 @@ initItemTypeElements = do
   where
     itemTypes = [Sword, Pickaxe, Shovel, Axe, Bow, Helmet, Chestplate, Leggings, Boots, FishingRod]
 
+itemTypeClicked :: Event -> Fay Bool
+itemTypeClicked ev = do
+    iT <- getEventElement ev >>= getAnvilValue 
+    filterMaterials iT
+    filterEnchants iT
+    return True
+
+-- Material selection logic.
+
 initMaterialElements :: Fay ()
 initMaterialElements = do
     elements <- getElementsByClass "material"
@@ -63,6 +82,20 @@ initMaterialElements = do
         bindSelectableEventListener element elements
   where
     materials = [Diamond, Iron, Gold, Chain, Leather, Stone, Wood]
+
+filterMaterials :: ItemType -> Fay ()
+filterMaterials itemType = do
+    let vMats = validMaterials itemType
+    matElements <- getElementsByClass "material"
+    shown <- forMaybeM matElements $ \matElem -> do
+        removeClass matElem "selected"
+        mat <- getAnvilValue matElem
+        if mat `elem` vMats
+            then showBlock matElem >> return (Just matElem)
+            else hideElement matElem >> return Nothing
+    addClass (head shown) "selected"
+
+-- Enchantment selection logic.
 
 initEnchantElements :: Fay ()
 initEnchantElements = do
@@ -87,6 +120,19 @@ initEnchantElements = do
         Sharpness, Smite, BaneOfArthropods, Knockback, FireAspect, Looting,
         Efficiency, SilkTouch, Unbreaking, Fortune,
         Power, Punch, Flame, Infinity, LuckOfTheSea, Lure]
+
+levelClicked :: [Element] -> [Element] -> Event -> Fay Bool
+levelClicked enchantElements levels ev = do
+    level <- getEventElement ev
+    selected <- hasClass level "selected"
+    if selected then removeClass level "selected"
+    else do
+        eT <- parentNode level >>= getAnvilValue
+        levels' <- getExclusiveEnchantLevels enchantElements levels eT 
+        forM_ levels' (flip removeClass "selected")
+        addClass level "selected"
+    return True
+  where
     getExclusiveEnchantLevels :: [Element] -> [Element] -> EnchantmentT -> Fay [Element]
     getExclusiveEnchantLevels enchantElements levels eT = do
         case exclusivityTagT eT of
@@ -104,44 +150,6 @@ initEnchantElements = do
                     then return . Just . tail . nodeListToArray =<< children e
                     else return Nothing
             Nothing -> return Nothing
-    levelClicked :: [Element] -> [Element] -> Event -> Fay Bool
-    levelClicked enchantElements levels ev = do
-        level <- getEventElement ev
-        selected <- hasClass level "selected"
-        if selected then removeClass level "selected"
-        else do
-            eT <- parentNode level >>= getAnvilValue
-            levels' <- getExclusiveEnchantLevels enchantElements levels eT 
-            forM_ levels' (flip removeClass "selected")
-            addClass level "selected"
-        return True
-
-editorItem :: Fay Item
-editorItem = do
-    itemType <- querySelector ".itemType.selected" >>= getAnvilValue
-    material <- querySelector ".material.selected" >>= getAnvilValue
-    name <- querySelector "#name" >>= getStringValue
-    let nnj = if name == "" then Right 0 else Left name
-    return $ makeItem itemType material [] nnj
-
-itemTypeClicked :: Event -> Fay Bool
-itemTypeClicked ev = do
-    iT <- getEventElement ev >>= getAnvilValue 
-    filterMaterials iT
-    filterEnchants iT
-    return True
-
-filterMaterials :: ItemType -> Fay ()
-filterMaterials itemType = do
-    let vMats = validMaterials itemType
-    matElements <- getElementsByClass "material"
-    shown <- forMaybeM matElements $ \matElem -> do
-        removeClass matElem "selected"
-        mat <- getAnvilValue matElem
-        if mat `elem` vMats
-            then showBlock matElem >> return (Just matElem)
-            else hideElement matElem >> return Nothing
-    addClass (head shown) "selected"
 
 filterEnchants :: ItemType -> Fay ()
 filterEnchants iT = do
